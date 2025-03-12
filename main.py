f88daa440a25ac2a68299f4df8c0e78c0e9df302
brisson0916 <brisson.lam@hotmail.com>
2025-03-12 14:18:38 +0800

    Update database URI

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ login_manager.init_app(app)
 class Base(DeclarativeBase):
     pass
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///anime_database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI",'sqlite:///anime_database.db')
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
